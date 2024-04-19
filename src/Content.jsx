1ae27c69dd488e6a8a94a0db0b60ac9a67c586ba
GageHoechst <hoechstg99@gmail.com>
2024-04-19 15:26:37 -0400

    added imports to content for signup, login, logout

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,13 @@
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
+
 export function Content() {
   return (
     <main>
-      <h1>Welcome to React</h1>
+      <Signup />
+      <Login />
+      <LogoutLink />
     </main>
   );
 }
