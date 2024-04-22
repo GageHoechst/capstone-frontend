b339308cdc36bef5fb4f3e16daa9571e08f1cf85
GageHoechst <hoechstg99@gmail.com>
2024-04-22 12:32:41 -0400

    made the index action show data from the backend

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,40 +1,25 @@
+import axios from "axios";
 import { Signup } from "./Signup";
 import { Login } from "./Login";
 import { LogoutLink } from "./LogoutLink";
 import { MoviesIndex } from "./MoviesIndex";
 import { MoviesShow } from "./MoviesShow";
 import { Modal } from "./Modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function Content() {
+  const [movies, setMovies] = useState([]);
+  const handleIndexMovies = () => {
+    console.log("handleIndexMovies");
+    axios.get("http://localhost:3000/movies.json").then((response) => {
+      console.log(response.data);
+      setMovies(response.data);
+    });
+  };
+  useEffect(handleIndexMovies, []);
   const [isMoviesShowVisible, setIsMoviesShowVisible] = useState(false);
   const [currentMovie, setCurrentMovie] = useState({});
-  const movies = [
-    {
-      id: 1,
-      name: "Placeholder Movie 1",
-      director: "Placeholder Director 1",
-      image_url: "https://placeholder.com/1",
-      description: "Placeholder description for Movie 1.",
-      run_time: "2h 30min",
-    },
-    {
-      id: 2,
-      name: "Placeholder Movie 2",
-      director: "Placeholder Director 2",
-      image_url: "https://placeholder.com/2",
-      description: "Placeholder description for Movie 2.",
-      run_time: "1h 45min",
-    },
-    {
-      id: 3,
-      name: "Placeholder Movie 3",
-      director: "Placeholder Director 3",
-      image_url: "https://placeholder.com/3",
-      description: "Placeholder description for Movie 3.",
-      run_time: "2h 10min",
-    },
-  ];
+
   const handleShowMovie = (movie) => {
     console.log("handleShowMovie", movie);
     setIsMoviesShowVisible(true);
