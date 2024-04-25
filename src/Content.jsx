67363e79beb80943e6e231c92c8892afc213ab0b
GageHoechst <hoechstg99@gmail.com>
2024-04-25 14:20:27 -0400

    added index to favorites

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,14 +4,18 @@ import { Login } from "./Login";
 import { LogoutLink } from "./LogoutLink";
 import { MoviesIndex } from "./MoviesIndex";
 import { MoviesShow } from "./MoviesShow";
+import { FavoritesIndex } from "./FavoritesIndex";
 import { FavoritesNew } from "./FavoritesNew";
 import { Modal } from "./Modal";
+import { Modal2 } from "./Modal2";
 import { useEffect, useState } from "react";
 
 export function Content() {
   const [movies, setMovies] = useState([]);
   const [isMoviesShowVisible, setIsMoviesShowVisible] = useState(false);
   const [currentMovie, setCurrentMovie] = useState({});
+  const [isFavoritesshowVisiible, setIsFavoritesShowVisible] = useState(false);
+  const [currentFavorite, setCurrentFavorite] = useState({});
 
   const handleIndexMovies = () => {
     console.log("handleIndexMovies");
@@ -20,6 +24,18 @@ export function Content() {
       setMovies(response.data);
     });
   };
+
+  const [favorites, setFavorites] = useState([]);
+
+  const handleIndexFavorites = () => {
+    console.log("handleIndexFavorites");
+    axios.get("http://localhost:3000/favorites.json").then((response) => {
+      console.log(response.data);
+      setFavorites(response.data);
+    });
+  };
+  useEffect(handleIndexFavorites, []);
+
   const handleCreateFavorite = (params, successCallback) => {
     console.log("handleCreateFavorite", params);
     axios.post("http://localhost:3000/favorites.json", params).then((response) => {
@@ -38,6 +54,12 @@ export function Content() {
   const handleClose = () => {
     console.log("handleClose");
     setIsMoviesShowVisible(false);
+    setIsFavoritesShowVisible(false);
+  };
+  const handleShowFavorite = (favorite) => {
+    console.log("handleShowFavorite", favorite);
+    setIsFavoritesShowVisible(true);
+    setCurrentFavorite(favorite);
   };
 
   return (
@@ -48,7 +70,10 @@ export function Content() {
       <LogoutLink />
       <FavoritesNew onCreateFavorite={handleCreateFavorite} />
       <MoviesIndex movies={movies} onShowMovie={handleShowMovie} />
-
+      <FavoritesIndex favorites={favorites} onShowFavorite={handleShowFavorite} />
+      <Modal2 show={isFavoritesshowVisiible} onClose={handleClose}>
+        + <h1>Test</h1>+{" "}
+      </Modal2>
       <Modal show={isMoviesShowVisible} onClose={handleClose}>
         <MoviesShow movie={currentMovie} />
       </Modal>
---
+++ src/FavoritesIndex.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+export function FavoritesIndex(props) {
+  return (
+    <div>
+      {props.favorites.map((favorite) => (
+        <div key={favorite.id}>
+          <h2>{favorite.user_id}</h2>
+          <h2>{favorite.movie_id}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
